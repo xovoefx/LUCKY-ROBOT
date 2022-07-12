3f85345f975a07b92d2ba17922dbe27946f12982
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <vijayrao1701@gmail.com>
2022-07-12 18:25:07 +0530

    Update antiflood.py

--- FallenRobot/modules/antiflood.py
+++ FallenRobot/modules/antiflood.py
@@ -419,7 +419,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Aɴᴛɪ-Fʟᴏᴏᴅ"
+__mod_name__ = "📛 ᴀɴᴛɪ-Fʟᴏᴏᴅ"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
