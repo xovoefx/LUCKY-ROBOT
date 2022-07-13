9ddb2d80a09f8a5498bd1e5892c7ff4f18b37c37
Snehashish Biswas <teamscenariox@gmail.com>
2022-07-13 23:57:09 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -79,7 +79,7 @@ uptime = get_readable_time((time.time() - StartTime))
 
 
 PM_START_TEXT = """
-ʜᴇʏ {}, [!](https://telegra.ph/file/9d57db2605497faa27903.jpg) {}\n
+ʜᴇʏw [!](https://telegra.ph/file/9d57db2605497faa27903.jpg) {}\n
 ᴍʏ ɴᴀᴍᴇ ɪs ʟᴜᴄᴋʏ ʀᴏʙᴏᴛ ,{} ✨,\n✦ ᴀ ᴘᴏᴡᴇʀғᴜʟʟ ғᴜʟʟ ғʟᴇᴅɢᴇ\nᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʀᴏʙᴏᴛ ᴡɪᴛʜ ᴏsᴍ\nsᴇᴄᴜʀɪᴛʏ ᴀɴᴅ ᴍᴏᴅᴜʟᴇs!!\nʜɪᴛ ᴏɴ /help ᴀɴᴅ ᴇxᴘʟᴏʀᴇʀ ʏᴏᴜʀsᴇʟғ.\n
 ─────────────────────
     ➻ ᴜᴘᴛɪᴍᴇ: {}
@@ -219,7 +219,7 @@ def start(update: Update, context: CallbackContext):
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
                     escape_markdown(first_name),
-                    START_IMG,
+#                    START_IMG,
                     escape_markdown(uptime),
                     sql.num_users(),
                     sql.num_chats()),
