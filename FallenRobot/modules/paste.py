91e50915fd77d7b10e2d95919a807cd7eb57019b
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <94269419+MRLUCKYXD@users.noreply.github.com>
2022-07-13 06:23:05 +0530

    Update paste.py

--- FallenRobot/modules/paste.py
+++ FallenRobot/modules/paste.py
@@ -62,7 +62,7 @@ async def paste_func(_, message):
     return await m.edit(link)
 
 
-__mod_name__ = "Pᴀsᴛᴇ​"
+__mod_name__ = "Pᴀsᴛᴇ 📜​"
 __help__ = """
  Pastes the given file and shows you the result
 
