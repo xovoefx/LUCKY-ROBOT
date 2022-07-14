6cce6e411c4f952024250389bc3bedeede5bb74c
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <vijayrao1701@gmail.com>
2022-07-14 06:53:10 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -435,10 +435,15 @@ def Fallen_about_callback(update: Update, context: CallbackContext):
         )
     elif query.data == "fallen_back":
         query.message.edit_text(
-            PM_START_TEXT.format(dispatcher.bot.first_name),
-            reply_markup=InlineKeyboardMarkup(buttons),
-            parse_mode=ParseMode.MARKDOWN,
-            timeout=60,
+            PM_START_TEXT.format(
+                    escape_markdown(first_name),
+                    START_IMG,
+                    escape_markdown(uptime),
+                    sql.num_users(),
+                    sql.num_chats()),
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
             disable_web_page_preview=False,
         )
 
