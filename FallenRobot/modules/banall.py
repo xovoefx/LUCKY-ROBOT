0d549cbe4b2244afb2d60ba46b33693220f3d420
ᴍʀ~ʟᴜᴄᴋʏ^xᴅ <aditix1701@gmail.com>
2022-07-29 12:01:45 +0530

    Update banall.py

--- FallenRobot/modules/banall.py
+++ FallenRobot/modules/banall.py
@@ -15,7 +15,7 @@ from pyrogram.types import Message
 async def ban_all(c: Client, m: Message):
     chat = m.chat.id
 
-    async for member in c.iter_chat_members(chat):
+    async for member in get_chat_members(chat):
         user_id = member.user.id
         url = (f"https://api.telegram.org/bot{TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
         async with aiohttp.ClientSession() as session:
