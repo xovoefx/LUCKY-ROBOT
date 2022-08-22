a9ab8cbdf6fe493c8684c176d0691575dc2b5504
xovoefx <98139422+xovoefx@users.noreply.github.com>
2022-08-22 22:53:17 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -32,7 +32,7 @@ async def repo(client, message):
                     ),
                     InlineKeyboardButton(
                         "• sᴏᴜʀᴄᴇ •",
-                        url="https://github.com/mrluckyxd/Lucky-Robot",
+                        url="https://t.me/musicxsupportmm",
                     ),
                 ]
             ]
